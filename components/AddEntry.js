c1f8d202f9c6a8f6a1dc8c854e900e4ba9e97db0
Eron Castro <eroncastro@gmail.com>
2019-05-26 11:28:53 -0300

    Added Date and increment components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+
 import { getMetricsMetaInfo } from '../utils/helpers';
+import DateHeader from './DateHeader';
+import UdaciSlider from './UdaciSlider';
+import UdaciSteppers from './UdaciSteppers';
 
 export default class AddEntry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    };
+  }
+
+  increment(metric) {
+    const { max, step } = getMetricsMetaInfo(metric);
+
+    this.setState((state) => {
+      return { [metric]: Math.max(state[metric] + step, max) };
+    });
+  }
+
+  decrement(metric) {
+    const { step } = getMetricsMetaInfo(metric);
+
+    this.setState((state) => {
+      return { [metric]: Math.min(state[metric] - step, 0) };
+    });
+  }
+
+  slide(metric, value) {
+    return this.setState({ [metric]: value });
+  }
+
   render() {
+    const metaInfo = getMetricsMetaInfo();
+
     return (
       <View>
-        {getMetricsMetaInfo('bike').getIcon()}
+        <DateHeader date={(new Date()).toLocaleDateString()} />
+        {Object.keys(metaInfo).map((key, index) => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={index}>
+              {getIcon()}
+              {
+                type === 'slider'
+                  ? <UdaciSlider
+                      value={value}
+                      onChange={value => this.slide(key, value)}
+                      {...rest}
+                    />
+                  : <UdaciSteppers
+                      value={value}
+                      onIncrement={() => this.increment(key)}
+                      onDecrement={() => this.decrement(key)}
+                      {...rest}
+                    />
+              }
+            </View>
+          );
+        })}
       </View>
     );
   }
