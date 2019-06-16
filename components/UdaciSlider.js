6b24756ba47711e9a7c8adf32c7d770377fe97e0
Eron Castro <eroncastro@gmail.com>
2019-06-16 16:45:08 -0300

    Added styles to slider, button and date

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { View, Text, Slider } from 'react-native';
+import { View, Text, Slider, StyleSheet } from 'react-native';
+import { gray } from '../utils/colors';
 
 export default function UdaciSlider(props) {
   const { max, unit, step, value, onChange } = props;
 
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={styles.slider}
         step={step}
         value={value}
         maximumValue={max}
         minimumValue={0}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 24 }}>{value}</Text>
+        <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  slider: {
+    flex: 1
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
